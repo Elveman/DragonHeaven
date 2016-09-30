2ded50e70adfdee146d1b787ea2c22c405ed89b0
Elveman <namevle@gmail.com>
2016-09-30 21:43:46 +0300

    Added random mod for Passive Aggressive

--- config/formats.js
+++ config/formats.js
@@ -356,6 +356,67 @@ exports.Formats = [
 	// Randomized Metas
 	///////////////////////////////////////////////////////////////////
 	{
+		name: "Passive Aggressive Random battle",
+		desc: [
+			"Type effectiveness applies to passive damage.",
+			"&bullet; <a href=\"https://www.smogon.com/forums/threads/3576924/\">Passive Aggressive</a>",
+		],
+		section: "RPC Metas",
+		team: 'random',
+		onDamage: function (damage, target, source, effect) {
+			let type;
+			switch (effect.id) {
+			default:
+				return;
+			case 'blacksludge':
+				type = 'Poison';
+				damage = target.maxhp / 8;
+				break;
+			case 'curse':
+			case 'nightmare':
+				type = 'Ghost';
+				damage = target.maxhp / 4;
+				break;
+			case 'leechseed':
+			case 'spikyshield':
+				type = 'Grass';
+				damage = target.maxhp / 8;
+				break;
+			case 'spikes':
+				type = 'Ground';
+				damage = target.maxhp / 2 / (5 - target.side.sideConditions.spikes.layers);
+				break;
+			case 'brn':
+				type = 'Fire';
+				damage = target.maxhp / 8;
+				break;
+			case 'psn':
+				type = 'Poison';
+				damage = target.maxhp / 8;
+				break;
+			case 'tox':
+				return this.clampIntRange(target.maxhp >> 4 - this.getEffectiveness('Poison', target), 1) * target.statusData.stage;
+			case 'partiallytrapped':
+				let effectData = target.volatiles.partiallytrapped;
+				damage = target.maxhp / (effectData.source.hasItem('bindingband') ? 6 : 8);
+				type = effectData.sourceEffect;
+				break;
+			case 'sandstorm':
+				type = 'Rock';
+				damage = target.maxhp / 16;
+				break;
+			case 'hail':
+				type = 'Ice';
+				damage = target.maxhp / 16;
+				break;
+			case 'recoil':
+				type = this.activeMove;
+				break;
+			}
+			return damage * Math.pow(2, this.getEffectiveness(type, target));
+		},
+	},
+	{
 		name: "Moody Metronome battle",
 		desc: ["Battle with 6 Moody Smeargles with Metronome. Pretty random."],
 		section: "RPC Metas",
